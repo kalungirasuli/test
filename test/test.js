6f04c9fd2c54d0efb20ef855c5eed4875498c734
kalungirasuli <kalungirasuli495@gmail.com>
2023-06-25 17:51:14 +0300

    auto checksthe code after updating multipy

--- test/test.js
+++ test/test.js
@@ -4,4 +4,5 @@ const multiply = require('../multipy.js');
 
 // assert.strictEqual(multiply(1, 1), 1);
 // assert.equal(multiply(2,2 ), 4);
-assert.equal(multiply(4, 4), 16);
+// assert.equal(multiply(4, 4), 16);
+assert.equal(multiply(23, 45), 23 * 45);
