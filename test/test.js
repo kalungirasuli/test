72cd6ab4ca0b61f84f3f00924beb6d1c5b56ff46
kalungirasuli <kalungirasuli495@gmail.com>
2023-06-25 17:30:03 +0300

    before updating multipy

--- test/test.js
+++ test/test.js
@@ -2,4 +2,5 @@ const { assert } = require('chai');
 
 const multiply = require('../multipy.js');
 
-assert.strictEqual(multiply(1, 1), 1);
+// assert.strictEqual(multiply(1, 1), 1);
+assert.equal(multiply(2,2 ), 4);
