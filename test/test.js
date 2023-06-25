3636f0fd40b7877c26cc904df5cbf551f38d17cf
kalungirasuli <kalungirasuli495@gmail.com>
2023-06-25 21:02:29 +0300

    test three

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 const { assert } = require('chai');
-
 const multiply = require('../multipy.js');
 
-// assert.strictEqual(multiply(1, 1), 1);
-// assert.equal(multiply(2,2 ), 4);
+// assert.equal(multiply(1, 1), 1);
+// assert.equal(multiply(3, 3), 9);
+// assert.equal(multiply(2, 2), 4);
 // assert.equal(multiply(4, 4), 16);
-assert.equal(multiply(23, 45), 23 * 45);
+// assert.equal(multiply(23, 45), 23 * 45);
